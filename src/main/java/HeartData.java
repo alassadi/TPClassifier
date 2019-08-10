662c6f934702209c0bc2cf77e9e412b67218b8a8
Abdulrahman Alassadi <alassadi@live.com>
2019-08-10 15:01:18 +0200

    imroved accuracy

--- src/main/java/HeartData.java
+++ src/main/java/HeartData.java
@@ -2,60 +2,52 @@
 /**
  * Created by Abood on 02-Aug-19.
  */
-public class HeartData
-{
-    public HeartData(String age, String cp, String restecg, String oldpeak, String slope, String ca, String thal, String exang, String sex) {
+public class HeartData {
+    //  age sex cp trestbps chol fbs restecg thalach exang oldpeak slope ca thal
+    String age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal;
+
+    public HeartData(String age, String sex, String cp, String trestbps, String chol, String fbs, String restecg, String thalach, String exang, String oldpeak, String slope, String ca, String thal) {
+        this.age = age;
+        this.sex = sex;
+        this.cp = cp;
+        this.trestbps = trestbps;
+        this.chol = chol;
+        this.fbs = fbs;
         this.restecg = restecg;
+        this.thalach = thalach;
+        this.exang = exang;
         this.oldpeak = oldpeak;
         this.slope = slope;
         this.ca = ca;
         this.thal = thal;
-        this.cp = cp;
-        this.exang = exang;
-        this.sex = sex;
-        this.age = age;
     }
 
-    //      6       9      10      11     12        8   1
-    String cp ,restecg, oldpeak, slope, ca, thal, exang, sex, age;
+    public String classify() {
 
-    public String detect () {
-
-        if(sex.equals("1") && (restecg.equals("1") || restecg.equals("2")) && (slope.equals("1") || slope.equals("2")) && ( thal.equals("2") )&& (Integer.parseInt (age.trim()) < 60) ){
-
-            return "1";
-        }
-        else if( ( cp.equals("1") ) && (restecg.equals("1") || restecg.equals("2")) && (Integer.parseInt (age.trim()) > 60) ){
-
-            return "1";
-        }
-
-        else if((slope.equals("2") || slope.equals("3")) && ( cp.equals("0") ) && (exang.equals("1"))){
-
-            return "1";
-        }
-
-        else if((restecg.equals("1") || restecg.equals("2")) && (slope.equals("2") || slope.equals("0"))&& ( Double.parseDouble(oldpeak.trim()) > 0) ){
-
-            return "1";
-        }
-        else if((ca.equals("2") || ca.equals("3")) && ( thal.equals("3") || thal.equals("1"))){
+        if (sex.equals("1") && cp.equals("0") && (Integer.parseInt(age.trim()) < 60)) {
 
             return "0";
-        }
-
-        else if(thal.equals("1")   || thal.equals("3") ){
+        } else if (cp.equals("0") && (exang.equals("1")) && (Double.parseDouble(oldpeak.trim()) > 0) && (Integer.parseInt(age.trim()) > 60)) {
 
             return "0";
-        }
-        else if(thal.equals("2")   && (ca.equals("2") || ca.equals("3") ) ){
+        } else if ((cp.equals("0") || cp.equals("1")) && (thal.equals("3"))) {
 
             return "0";
-        }
-        else if(thal.equals("2")){
+        } else if ((cp.equals("0")) && Integer.parseInt(ca) > 1) {
+
+            return "0";
+        } else if ((cp.equals("2")) && (exang.equals("0"))) {
 
             return "1";
-        }
-        else return "3";
+        } else if ((thal.equals("2")) && (restecg.equals("0"))) {
+
+            return "1";
+        } else if (ca.equals("0")) {
+
+            return "1";
+        } else if ((Integer.parseInt(trestbps.trim()) > 140) && (Integer.parseInt(chol.trim()) > 200)) {
+
+            return "0";
+        } else return "1";
     }
 }
--- src/main/java/TProgram.java
+++ src/main/java/TProgram.java
@@ -1,5 +1,6 @@
 import com.opencsv.CSVReader;
 import java.io.FileReader;
+import java.text.DecimalFormat;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -10,42 +11,51 @@ import java.util.ArrayList;
 public class TProgram {
     public static void main(String[] args) {
         Instant start = Instant.now();
-
-        ArrayList <String> results = new ArrayList();
-        int acc = 0;
-        int acc1 = 0;
-            try {
-
-                CSVReader reader = new CSVReader(new FileReader("src/data/heart.csv"));
-                String [] nextLine;
-                while ((nextLine = reader.readNext()) != null) {
-                    // nextLine[] is an array of values from the line
-                    // cp = 2,  restecg 6, oldpeak 9  ..slope  10 ..  ca   11 ..  thal  12
-                    HeartData heartData = new HeartData(nextLine[0],nextLine[2],nextLine[6],nextLine[9], nextLine[10],
-                            nextLine[11], nextLine[12], nextLine[8], nextLine[1]);
-                    results.add(heartData.detect());
-                    if(nextLine[13].equals(heartData.detect())) {
-                        acc++;
-
-                    }
-                    if(heartData.detect().equals("3")) {
-                        acc1++;
-
-                    }
+        ArrayList<String> results = new ArrayList();
+        double acc = 0;
+        double tp = 0, tn = 0, fp = 0, fn = 0;
+        int nono = 0;
+        try {
+            CSVReader reader = new CSVReader(new FileReader("src/data/heart-original.csv"));
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                HeartData heartData = new HeartData(nextLine[0], nextLine[1], nextLine[2], nextLine[3], nextLine[4],
+                        nextLine[5], nextLine[6], nextLine[7], nextLine[8], nextLine[9],
+                        nextLine[10], nextLine[11], nextLine[12]);
+                results.add(heartData.classify());
+                if (nextLine[13].equals(heartData.classify())) {
+                    acc++;
+                    if (nextLine[13].equals("0")) {
+                        tn++;
+                    } else tp++;
+                } else if (nextLine[13].equals("0")) {
+                    fp++;
+                } else {
+                    fn++;
                 }
-
-                int total =( acc * 100 )/1508940;
-                System.out.println(total);
-                Instant end = Instant.now();
-                System.out.println(Duration.between(start, end));
-                System.out.println(acc1);
-
+                if (heartData.classify().equals("3")) {
+                    nono++;
+                }
             }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-
-
+            DecimalFormat decimalFormat = new DecimalFormat("#.00");
+            double accuracy = (acc * 100) / 303;
+            System.out.println("The classification accuracy is: " + Double.valueOf(decimalFormat.format(accuracy)) + "%");
+            double precision = tp / (tp + fp);
+            double recall = tp / (tp + fn);
+            System.out.println("The classification precision for class 0 is: " + Double.valueOf(decimalFormat.format(100 * precision)) + "%");
+            System.out.println("The classification recall for class 0 is: " + Double.valueOf(decimalFormat.format(100 * recall)) + "%");
+            precision = tn / (tn + fn);
+            recall = tn / (tn + fp);
+            System.out.println("The classification precision for class 1: " + Double.valueOf(decimalFormat.format(100 * precision)) + "%");
+            System.out.println("The classification recall for class 1: " + Double.valueOf(decimalFormat.format(100 * recall)) + "%");
+            Instant end = Instant.now();
+            System.out.println("The execution time: " + Duration.between(start, end));
+            System.out.println(nono);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+
+    }
 
 }
